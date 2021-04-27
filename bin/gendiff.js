4b2fd590a229a8fc9bff5404ee7b4f09fbfa97e8
anatolii-serzhantov <serzhantov.anatoly@gmail.com>
2021-04-27 19:01:18 +0300

    add -f (format) option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ const program = new Command();
 
 program
 .version('0.0.1', '-V, --version', 'output the version number')
-.description('Compares two configuration files and shows a difference.');
-program.parse();
+.description('Compares two configuration files and shows a difference.')
+.arguments('<filepath1> <filepath2>')
+.option('-f, --format [type]', 'output format')
+program.parse(process.argv);
+
 console.log(1);
