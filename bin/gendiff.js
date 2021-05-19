9dee8b3af2437d601cc13fae8de2be4314866596
anatolii-serzhantov <serzhantov.anatoly@gmail.com>
2021-05-19 17:56:27 +0300

    add file reading logic

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,62 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
-import _ from 'lodash';
-const program = new Command();
+import genDiff from '../src/index.js';
 
+const program = new Command();
 program
 .version('0.0.1', '-V, --version', 'output the version number')
 .description('Compares two configuration files and shows a difference.')
 .arguments('<filepath1> <filepath2>')
 .option('-f, --format [type]', 'output format')
+.action((filepath1, filepath2) => {
+  console.log(genDiff(filepath1, filepath2));
+});
 program.parse(process.argv);
-
-const obj1 = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": false
-}
-
-const obj2 = {
-    "timeout": 20,
-    "verbose": true,
-    "host": "hexlet.io"
-}
-
-const genDiff = (data1, data2) => {
-    const keys1 = _.keys(data1);
-    const keys2 = _.keys(data2);
-    const keys = _.union(keys1, keys2);
-   
-    let listOfDifferences = [];
-    for (const key of keys) {
-      if (!_.has(data1, key)) {
-        listOfDifferences.push(`  + ${key}: ${data2[key]}`);
-      } else if (!_.has(data2, key)) {
-        listOfDifferences.push(`  - ${key}: ${data1[key]}`);
-      } else if (data1[key] !== data2[key]) {
-        listOfDifferences.push(`  - ${key}: ${data1[key]}`);
-        listOfDifferences.push(`  + ${key}: ${data2[key]}`);
-      } else {
-        listOfDifferences.push(`    ${key}: ${data1[key]}`);
-      }
-    }
-
-    listOfDifferences.sort(function(a, b){
-        if (a[4] < b[4]) {
-          return -1;
-        }
-        if (a[4] > b[4]) {
-          return 1;
-        }
-        return 0;
-    })
-
-    const str = listOfDifferences.join("\r\n");
-    //console.log(`{\r\n${str}\r\n}`)
-    return console.log(`{\r\n${str}\r\n}`);
-}
-
-genDiff(obj1, obj2);
-
-export default genDiff;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+const readFile = (pathName) => {
+    const fullPath = path.resolve(process.cwd(), pathName);
+    const data = fs.readFileSync(fullPath).toString();
+    return data;
+  };
+  
+const genDiff = (data1, data2) => {
+    const path1 = readFile(data1);
+    const obj1 = JSON.parse(path1);
+    const path2 = readFile(data2);
+    const obj2 = JSON.parse(path2);
+    const keys1 = _.keys(obj1);
+    const keys2 = _.keys(obj2);
+    const keys = _.union(keys1, keys2);
+   
+    let listOfDifferences = [];
+    for (const key of keys) {
+      if (!_.has(obj1, key)) {
+        listOfDifferences.push(`  + ${key}: ${obj2[key]}`);
+      } else if (!_.has(obj2, key)) {
+        listOfDifferences.push(`  - ${key}: ${obj1[key]}`);
+      } else if (obj1[key] !== obj2[key]) {
+        listOfDifferences.push(`  - ${key}: ${obj1[key]}`);
+        listOfDifferences.push(`  + ${key}: ${obj2[key]}`);
+      } else {
+        listOfDifferences.push(`    ${key}: ${obj1[key]}`);
+      }
+    }
+
+    listOfDifferences.sort(function(a, b){
+        if (a[4] < b[4]) {
+          return -1;
+        }
+        if (a[4] > b[4]) {
+          return 1;
+        }
+        return 0;
+    })
+
+    const str = listOfDifferences.join("\r\n");
+    return `{\r\n${str}\r\n}`;
+}
+
+export default genDiff;
