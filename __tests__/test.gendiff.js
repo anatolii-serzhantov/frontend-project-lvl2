adeb886359b81014ceeb5d740a9f4e6363e46c36
anatolii-serzhantov <serzhantov.anatoly@gmail.com>
2021-06-09 19:06:14 +0300

    fix: tests

--- __tests__/test.gendiff.js
+++ __tests__/test.gendiff.js
@@ -6,13 +6,6 @@ const fileOneJson = path.resolve(process.cwd(), `./__tests__/__fixtures__/firstF
 const fileTwoJson = path.resolve(process.cwd(), `./__tests__/__fixtures__/secondFile.json`);
 
 test('test difference between two json files', () => {
-  const expectedDifference = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+  const expectedDifference = `{\r\n  - follow: false\r\n    host: hexlet.io\r\n  - proxy: 123.234.53.22\r\n  - timeout: 50\r\n  + timeout: 20\r\n  + verbose: true\r\n}`;
 expect(genDiff(fileOneJson, fileTwoJson)).toEqual(expectedDifference);
 });
